086a8359928701e6dd2517ff070bfdb7eb3709e5
Joefrey B. Sacay <cc.joefrey.sacay@cvsu.edu.ph>
2026-05-13 10:20:42 +0800

    Make promo migrations idempotent

--- backend/migrations/202605110001-add-additional-promo-ids-to-enrollments.js
+++ backend/migrations/202605110001-add-additional-promo-ids-to-enrollments.js
@@ -1,15 +1,37 @@
 "use strict";
 
+async function hasTable(queryInterface, tableName) {
+  try {
+    await queryInterface.describeTable(tableName);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+async function hasColumn(queryInterface, tableName, columnName) {
+  if (!(await hasTable(queryInterface, tableName))) {
+    return false;
+  }
+
+  const definition = await queryInterface.describeTable(tableName);
+  return Object.prototype.hasOwnProperty.call(definition, columnName);
+}
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn("Enrollments", "additional_promo_offer_ids", {
-      type: Sequelize.JSON,
-      allowNull: true,
-      defaultValue: null,
-    });
+    if (!(await hasColumn(queryInterface, "Enrollments", "additional_promo_offer_ids"))) {
+      await queryInterface.addColumn("Enrollments", "additional_promo_offer_ids", {
+        type: Sequelize.JSON,
+        allowNull: true,
+        defaultValue: null,
+      });
+    }
   },
 
   down: async (queryInterface/*, Sequelize*/) => {
-    await queryInterface.removeColumn("Enrollments", "additional_promo_offer_ids");
+    if (await hasColumn(queryInterface, "Enrollments", "additional_promo_offer_ids")) {
+      await queryInterface.removeColumn("Enrollments", "additional_promo_offer_ids");
+    }
   },
 };
--- backend/migrations/202605110002-add-additional-promos-amount-to-enrollments.js
+++ backend/migrations/202605110002-add-additional-promos-amount-to-enrollments.js
@@ -1,15 +1,37 @@
 "use strict";
 
+async function hasTable(queryInterface, tableName) {
+  try {
+    await queryInterface.describeTable(tableName);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+async function hasColumn(queryInterface, tableName, columnName) {
+  if (!(await hasTable(queryInterface, tableName))) {
+    return false;
+  }
+
+  const definition = await queryInterface.describeTable(tableName);
+  return Object.prototype.hasOwnProperty.call(definition, columnName);
+}
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn("Enrollments", "additional_promos_amount", {
-      type: Sequelize.DECIMAL(10, 2),
-      allowNull: true,
-      defaultValue: 0,
-    });
+    if (!(await hasColumn(queryInterface, "Enrollments", "additional_promos_amount"))) {
+      await queryInterface.addColumn("Enrollments", "additional_promos_amount", {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: true,
+        defaultValue: 0,
+      });
+    }
   },
 
   down: async (queryInterface/*, Sequelize*/) => {
-    await queryInterface.removeColumn("Enrollments", "additional_promos_amount");
+    if (await hasColumn(queryInterface, "Enrollments", "additional_promos_amount")) {
+      await queryInterface.removeColumn("Enrollments", "additional_promos_amount");
+    }
   },
 };
